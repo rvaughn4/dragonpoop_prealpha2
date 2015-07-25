c23d0ea2760aaf618d960509bb4d58824ce5490a
rvaughn4 <richard@richards-Mac-mini.local>
2015-07-25 08:25:14 -0500

    1
    1

---
+++ dragonpoop_prealpha2/dragonpoop_prealpha2/dragonpoop/renderer/renderers.h
@@ -0,0 +1,11 @@
+
+#ifndef dragonpoop_renderers_h
+#define dragonpoop_renderers_h
+
+#include "renderer.h"
+#include "renderer_ref.h"
+#include "renderer_readlock.h"
+#include "renderer_writelock.h"
+#include "renderer_task.h"
+
+#endif
